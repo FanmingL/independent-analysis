1f1b655e8cfc13115086e3f8c74473b028dffb66
FanmingL <151190065@smail.nju.edu.cn>
2018-02-07 23:39:00 +0800

    finished by MATLAB

--- my_RealTimeIVA.m
+++ my_RealTimeIVA.m
@@ -7,19 +7,19 @@ close all;
 fft_length          = 256;                      %unit is sample
 window_length       = fft_length;               %unit is sample
 shift_size          = 64;                       %unit is sample
-beta                = 0.15;                      %smoothing factor
-reverbration_time   = 200;                      %unit is ms
+beta                = 0.5;                     %smoothing factor
+reverbration_time   = 250;                      %unit is ms
 windows             = hanning(fft_length);      %hanning window
 expect_sample_rate  = 8000;                     %8kHz sample rate
 yita                = 1.4;                      %gridant desent coeff
 start_simulate_point= 120000;                   %point start used to simulate
-simulation_length   = 20000;                    %length used to simulate, unit is ms
+simulation_length   = 60000;                    %length used to simulate, unit is ms
 c                   = 344;                      % Sound velocity(m/s)
-reciver_position1   = [ 2 2 2 ];                % Receiver position[ x y z ](m)
-source_position1    = [ 2 2.5 1 ];              % Source position[ x y z ](m)
-reciver_position2   = [ 2 2.1 2 ];              % Receiver position[ x y z ](m)
-source_position2    = [ 3 2.5 3 ];              % Source position[ x y z ](m)
-room_size           = [ 5 4 6 ];                % Room dimensions[ x y z ](m)
+reciver_position1   = [ 2.0 2.0 2.4 ];          % Receiver position[ x y z ](m)
+source_position1    = [ 2.0 2.4 2.0 ];          % Source position[ x y z ](m)
+reciver_position2   = [ 2.0 2.0 1.9 ];          % Receiver position[ x y z ](m)
+source_position2    = [ 2.0 1.6 2.7 ];          % Source position[ x y z ](m)
+room_size           = [ 5.0 4.0 6.0 ];          % Room dimensions[ x y z ](m)
 number_of_samples   = reverbration_time/1000 ...
                         * expect_sample_rate;   % Number of samples
 
@@ -55,10 +55,14 @@ G = zeros(2,2,fft_length);
 for i=1:fft_length
     G(1,1,i)=1;G(2,2,i)=1;
 end
-ksi             = ones(fft_length,1);
-phi             = ones(fft_length,2);
-R               = zeros(2,2,fft_length);
-audio_estimate  = zeros(fft_length,2);
+ksi                 = ones(fft_length,1);
+phi                 = ones(fft_length,2);
+R                   = zeros(2,2,fft_length);
+audio_estimate      = zeros(fft_length,2);
+number_of_overlap   = floor(fft_length/shift_size)+1;
+estimate_out1       = zeros(length(observation1),1);
+estimate_out2       = zeros(length(observation2),1);
+norm_coeff          = zeros(fft_length,1);
 tic;
 %//6 iterator
 for n = 1:(floor((length(observation1)-window_length)/shift_size))       % at Nth times
@@ -70,22 +74,27 @@ for n = 1:(floor((length(observation1)-window_length)/shift_size))       % at Nt
     audio_cut_fft  = [audio_cut1_fft, audio_cut2_fft];
     for k = 1:fft_length
         audio_estimate(k,:) = (G(:,:,k)* (audio_cut_fft(k,:)).').';
+        norm_coeff(k)       = norm(G(:,:,k));
     end
     
     phi = audio_estimate ./ (sqrt(diag((audio_estimate)'*(audio_estimate)))');
-%    phi(:,1) = audio_cut1_fft  / sqrt(audio_cut1_fft'*audio_cut1_fft);
-%    phi(:,2) = audio_cut2_fft  / sqrt(audio_cut2_fft'*audio_cut2_fft);
     for k = 1:fft_length
        R(:,:,k)   = (audio_estimate(k,:)'*phi(k,:)).';
        gradient_k = (diag(diag(R(:,:,k)))-R(:,:,k))*G(:,:,k);
        G(:,:,k)   = G(:,:,k) + yita/sqrt(ksi(k))*gradient_k;
        ksi(k)     = beta * ksi(k) + (1-beta)  *(audio_cut_fft(k,:)*audio_cut_fft(k,:)')/2;
-       audio_estimate(k,:) = audio_estimate(k,:) / (abs(G(1,1,k))*abs(G(2,2,k)));
+       audio_estimate(k,:) = audio_estimate(k,:) / norm_coeff(k);
     end
+    estimate_out1((n-1)*shift_size+1:(n-1)*shift_size+window_length)=...
+                  ifft(audio_estimate(:,1))+estimate_out1((n-1)*shift_size...
+                    +1:(n-1)*shift_size+window_length);
+    estimate_out2((n-1)*shift_size+1:(n-1)*shift_size+window_length)=...
+                  ifft(audio_estimate(:,2))+estimate_out2((n-1)*shift_size...
+                    +1:(n-1)*shift_size+window_length);
 end
 
 toc
-
+contrast_plot(estimate_out1,estimate_out2,expect_sample_rate,'sperated');
 function contrast_plot(x,y,fs,title_str)
     figure;
     subplot(2,1,1);
